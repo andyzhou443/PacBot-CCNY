1f4273e7b285e2414b797cacfec1fc99c815d2f7
Catherine <catherine.matta22399@gmail.com>
2022-03-18 20:15:54 -0400

    DFS for barebones grid complete, need backtracking method

---
+++ Pacbot_code/pacbot_algo/grid.py
@@ -0,0 +1,79 @@
+
+I = 1
+o = 2
+e = 3
+O = 4
+n = 5
+c = 6                        
+# Grid enums
+# o = normal pellet, e = empty space, O = power pellet, c = cherry position
+# I = wall, n = ghost chambers
+
+#       bottom left of pacman board                             # top left of pacman board
+grid = [[I,I,I,I,I,I,I,I,I,I,I,I,e,e,e,e,e,e,e,e,e,I,I,I,I,I,I,I,I,I,I], # 0
+        [I,o,o,o,o,I,I,O,o,o,o,I,e,e,e,e,e,e,e,e,e,I,o,o,o,o,o,O,o,o,I],
+        [I,o,I,I,o,I,I,o,I,I,o,I,e,e,e,e,e,e,e,e,e,I,o,I,I,o,I,I,I,o,I],
+        [I,o,I,I,o,o,o,o,I,I,o,I,e,e,e,e,e,e,e,e,e,I,o,I,I,o,I,e,I,o,I],
+        [I,o,I,I,o,I,I,I,I,I,o,I,e,e,e,e,e,e,e,e,e,I,o,I,I,o,I,e,I,o,I],
+        [I,o,I,I,o,I,I,I,I,I,o,I,I,I,I,I,I,I,I,I,I,I,o,I,I,o,I,I,I,o,I], # 5
+        [I,o,I,I,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,I],
+        [I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,e,I,I,I,I,I,I,I,I,o,I,I,I,o,I],
+        [I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,e,I,I,I,I,I,I,I,I,o,I,e,I,o,I],
+        [I,o,I,I,o,o,o,o,I,I,o,e,e,e,e,e,e,e,e,e,I,I,o,o,o,o,I,e,I,o,I],
+        [I,o,I,I,o,I,I,o,I,I,o,I,I,e,I,I,I,I,I,e,I,I,o,I,I,o,I,e,I,o,I], # 10
+        [I,o,I,I,o,I,I,o,I,I,o,I,I,e,I,n,n,n,I,e,I,I,o,I,I,o,I,I,I,o,I],
+        [I,o,o,o,o,I,I,o,o,o,o,I,I,e,I,n,n,n,I,e,e,e,o,I,I,o,o,o,o,o,I],
+        [I,o,I,I,I,I,I,e,I,I,I,I,I,e,I,n,n,n,n,e,I,I,I,I,I,o,I,I,I,I,I],
+        [I,o,I,I,I,I,I,e,I,I,I,I,I,e,I,n,n,n,n,e,I,I,I,I,I,o,I,I,I,I,I], # starting point here
+        [I,o,o,o,o,I,I,o,o,o,o,I,I,e,I,n,n,n,I,e,e,e,o,I,I,o,o,o,o,o,I], # 15
+        [I,o,I,I,o,I,I,o,I,I,o,I,I,e,I,n,n,n,I,e,I,I,o,I,I,o,I,I,I,o,I],
+        [I,o,I,I,o,I,I,o,I,I,o,I,I,e,I,I,I,I,I,e,I,I,o,I,I,o,I,e,I,o,I],
+        [I,o,I,I,o,o,o,o,I,I,o,e,e,e,e,e,e,e,e,e,I,I,o,o,o,o,I,e,I,o,I],
+        [I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,e,I,I,I,I,I,I,I,I,o,I,e,I,o,I],
+        [I,o,I,I,I,I,I,o,I,I,o,I,I,I,I,I,e,I,I,I,I,I,I,I,I,o,I,I,I,o,I], # 20
+        [I,o,I,I,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,I],
+        [I,o,I,I,o,I,I,I,I,I,o,I,I,I,I,I,I,I,I,I,I,I,o,I,I,o,I,I,I,o,I],
+        [I,o,I,I,o,I,I,I,I,I,o,I,e,e,e,e,e,e,e,e,e,I,o,I,I,o,I,e,I,o,I],
+        [I,o,I,I,o,o,o,o,I,I,o,I,e,e,e,e,e,e,e,e,e,I,o,I,I,o,I,e,I,o,I],
+        [I,o,I,I,o,I,I,o,I,I,o,I,e,e,e,e,e,e,e,e,e,I,o,I,I,o,I,I,I,o,I], # 25
+        [I,o,o,o,o,I,I,O,o,o,o,I,e,e,e,e,e,e,e,e,e,I,o,o,o,o,o,O,o,o,I],
+        [I,I,I,I,I,I,I,I,I,I,I,I,e,e,e,e,e,e,e,e,e,I,I,I,I,I,I,I,I,I,I]]
+#        |         |         |         |         |         |         |   top right of pacman board
+#        0         5        10        15       20         25       30
+
+row = 27
+col = 30
+print(grid[row][col])
+
+
+def dfs(x, y):
+        stack = []
+        stack.append([x,y])
+        value = ""
+        current = 0
+        while(len(stack) != 0):
+                print(current)
+                current = stack.pop()
+                x = current[0]
+                y = current[1]
+                value = grid[x][y]
+                grid[x][y]="X"
+                if((x+1<row or y<col) and grid[x+1][y] not in [n, I] and grid[x+1][y] !="X"):
+                        value = grid[x+1][y]
+                        stack.append([x+1,y])
+                if((x<row or y+1<col) and grid[x][y+1] not in [n, I] and grid[x][y+1] !="X"):
+                        value = grid[x][y+1]
+                        stack.append([x,y+1])
+                if((x-1>=1 or y>=1) and grid[x-1][y] not in [n, I] and grid[x-1][y] !="X"):
+                        value = grid[x-1][y]
+                        stack.append([x-1,y])
+                if((x>=1 or y-1>=1) and grid[x][y-1] not in [n, I] and grid[x][y-1] !="X"):
+                        value = grid[x][y-1]
+                        stack.append([x,y-1])
+                
+
+dfs(14, 7)
+
+f = open("aftergrid.txt", "w")
+f.write(str(grid).replace('],','],\n'))
+f.close()
