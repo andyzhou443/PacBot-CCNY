2a539a87d33f481c80a37d0620b6b6c5f52784bd
Benjamin <47288387+nimajneben@users.noreply.github.com>
2022-04-18 13:28:48 -0400

    Create motor_coordinator.cpp

---
+++ Arduino_code/motor_coordinator.cpp
@@ -0,0 +1,14 @@
+namespace m_coord
+{
+    private:
+    bool ready;
+    int current_direction;
+    int current_speed;
+
+    public:
+    void move(double distance, double angle)
+    {
+        ;
+    }
+
+}
