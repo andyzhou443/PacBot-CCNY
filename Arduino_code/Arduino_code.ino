b213c2cb4fe3dd4e22801a4b9cbb5d30f0b92a6c
Benjamin <47288387+nimajneben@users.noreply.github.com>
2022-04-18 01:37:47 -0400

    td: partially add command str processing

--- Arduino_code/Arduino_code.ino
+++ Arduino_code/Arduino_code.ino
@@ -2,38 +2,42 @@
 #include "motor.hpp"
 
 void setup() {
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
 
-  // Setup serial port
-  COM serial(9600);
-  
-  // Setup pin directions
-  pinMode(enc_1a, INPUT); pinMode(enc_1b, INPUT);
-  pinMode(enc_2a, INPUT); pinMode(enc_2b, INPUT);
-  pinMode(enc_3a, INPUT); pinMode(enc_3b, INPUT); 
-  pinMode(enc_4a, INPUT); pinMode(enc_4b, INPUT);
+    // Setup serial port
+    Serial.begin(115200);
 
-  pinMode(motor_1a,OUTPUT); pinMode(motor_1b,OUTPUT);
-  pinMode(motor_2a,OUTPUT); pinMode(motor_2b,OUTPUT);
-  pinMode(motor_3a,OUTPUT); pinMode(motor_3b,OUTPUT);
-  pinMode(motor_4a,OUTPUT); pinMode(motor_4b,OUTPUT);
-  
-  // Setup interfacing objects
-  Motor m1(motor_1a,motor_1b);
-  Motor m2(motor_2a,motor_2b);
-  Motor m3(motor_3a,motor_3b);
-  Motor m4(motor_4a,motor_4b);
+    // Setup pin directions
+    pinMode(enc_1a, INPUT); pinMode(enc_1b, INPUT);
+    pinMode(enc_2a, INPUT); pinMode(enc_2b, INPUT);
+    pinMode(enc_3a, INPUT); pinMode(enc_3b, INPUT); 
+    pinMode(enc_4a, INPUT); pinMode(enc_4b, INPUT);
+
+    pinMode(motor_1a,OUTPUT); pinMode(motor_1b,OUTPUT);
+    pinMode(motor_2a,OUTPUT); pinMode(motor_2b,OUTPUT);
+    pinMode(motor_3a,OUTPUT); pinMode(motor_3b,OUTPUT);
+    pinMode(motor_4a,OUTPUT); pinMode(motor_4b,OUTPUT);
+
+    // Setup interfacing objects
+    Motor m1(motor_1a,motor_1b);
+    Motor m2(motor_2a,motor_2b);
+    Motor m3(motor_3a,motor_3b);
+    Motor m4(motor_4a,motor_4b);
 
 
-  
+    // Setup global coordinate object
+    coord = {13,7};
+
+
+    while (!Serial);
     Serial.write("Teensy OK\n");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
 
-  //  Read serial
-  char command_buffer[15];
+    //  Read serial
+    char command_buffer[15];
     int buffer_size = 0;
     buffer_size = 
         Serial.readBytesUntil('\n',command_buffer,15);
@@ -42,53 +46,41 @@ void loop() {
 
     // Echo Input
     if (buffer_size > 0)
-  {
+    {
         Serial.write(command_buffer,buffer_size);
         Serial.write('\n');
     }
-    command_buffer[++i] = '\0';
-    
-    // consume newline
-    if (Serial.peek() == '\n' || Serial.peek() == '\r')
-      Serial.read();
-    
-    Serial.println(command_buffer);    
-    
-    // Parse Code
 
-    // Movement commands
-    if (strcmp(command_buffer, DIR_DOWN) == 0)
+    // Parse P-Code
     {
+        if(isdigit(command_buffer[0]))
+        {
+            // Movement code
+            if(strcmp(command_buffer[0],"0") == 0)
+                Serial.write("0 MOVE FORWARD\n");
+            else if(strcmp(command_buffer[0],"1") == 0)
+                Serial.write("1 MOVE BACKWARD\n");
+            else if(strcmp(command_buffer[0],"2") == 0)
+                Serial.write("2 MOVE LEFT\n");
+            else if(strcmp(command_buffer[0],"3") == 0)
+                Serial.write("3 MOVE RIGHT\n");
+            else
+                Serial.write("Invalid Movement Command\n");
+            
 
-    }
-    else if (strcmp(command_buffer, DIR_LEFT) == 0)
-    {
-    }
-    else if (strcmp(command_buffer, DIR_UP) == 0)
-    {
+            char temp_x[2];
+            char temp_y[2];
+            strncpy(temp_x, command_buffer+2, 2);
+            strncpy(temp_y, command_buffer+5, 2);
+            coord.x = atoi(temp_x);
+            coord.y = atoi(temp_y);
 
-    }
-    else if (strcmp(command_buffer,DIR_DOWN) == 0)
-    {
+            Serial.write("Current coordinates: ");
+            Serial.print(temp_x); Serial.print(temp_y);
+            Serial.write("\n");
 
-    }
 
-    // Test commands
-    else if (strcmp(command_buffer,"T1") == 0)
-    {
-      Serial.println("Motor TEST");
+        };
+        
     }
-    else if (strcmp(command_buffer,"T2") == 0)
-    {
-      Serial.println("Encoder TEST");
-    }
-    else if (strcmp(command_buffer,"T3") == 0)
-    {
-      Serial.println("ToF TEST");
-    }
-    else if(strcmp(command_buffer,"T4") == 0)
-    {
-      Serial.println("IMU TEST");
-    }
-  }
 }
