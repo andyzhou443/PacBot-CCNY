5646dcd7ab5757c32744d901f2f476f59c18115f
Benjamin <47288387+nimajneben@users.noreply.github.com>
2022-04-21 16:43:57 -0400

    fix encoder definition bug

--- Arduino_code/encoder_funcs.hpp
+++ Arduino_code/encoder_funcs.hpp
@@ -8,9 +8,9 @@ namespace enc_f
     // Wheel radius (in mm)
     extern const int WHEEL_RADIUS;
 
-    long get_ticks(Encoder& e,float d);
-    int get_speed_inch(Encoder& e, float d);
-    int get_speed_mm(Encoder& e, float d);
+    long get_ticks(Encoder& e,int d);
+    int get_speed_inch(Encoder& e, int d);
+    int get_speed_mm(Encoder& e, int d);
 } // enc_f namespace
 
 #endif //ENC_FUNC
