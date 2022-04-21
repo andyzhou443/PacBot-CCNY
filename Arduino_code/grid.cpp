8f74c7ddbeb7650ed7637fc9a45b1de683f02644
Benjamin <47288387+nimajneben@users.noreply.github.com>
2022-04-21 16:44:19 -0400

    fix definition

--- Arduino_code/grid.cpp
+++ Arduino_code/grid.cpp
@@ -12,7 +12,7 @@ int c = 6;
 //line 29 is starting point
 
 //       bottom left of pacman board                             # top left of pacman board
- int grid[28][31] = 
+const int grid[28][31] = 
            {{I,I,I,I,I,I,I,I,I,I,I,I,e,e,e,e,e,e,e,e,e,I,I,I,I,I,I,I,I,I,I}, 
             {I,o,o,o,o,I,I,O,o,o,o,I,e,e,e,e,e,e,e,e,e,I,o,o,o,o,o,O,o,o,I},
             {I,o,I,I,o,I,I,o,I,I,o,I,e,e,e,e,e,e,e,e,e,I,o,I,I,o,I,I,I,o,I},
@@ -40,6 +40,6 @@ int c = 6;
             {I,o,I,I,o,o,o,o,I,I,o,I,e,e,e,e,e,e,e,e,e,I,o,I,I,o,I,e,I,o,I},
             {I,o,I,I,o,I,I,o,I,I,o,I,e,e,e,e,e,e,e,e,e,I,o,I,I,o,I,I,I,o,I}, 
             {I,o,o,o,o,I,I,O,o,o,o,I,e,e,e,e,e,e,e,e,e,I,o,o,o,o,o,O,o,o,I},
-            {I,I,I,I,I,I,I,I,I,I,I,I,e,e,e,e,e,e,e,e,e,I,I,I,I,I,I,I,I,I,I}}
+            {I,I,I,I,I,I,I,I,I,I,I,I,e,e,e,e,e,e,e,e,e,I,I,I,I,I,I,I,I,I,I}};
 //       |         |         |         |         |         |         |   top right of pacman board
 //       0         5        10        15       20         25       30
