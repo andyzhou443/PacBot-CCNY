4b599e08254c12becff592698eadb51769c2c6ca
dylanlinux <accounts@dylankirdahy.nyc>
2022-04-23 05:27:25 -0400

    added error parsing for TOF

--- Arduino_code/Arduino_code.ino
+++ Arduino_code/Arduino_code.ino
@@ -269,6 +269,34 @@ void loop() {
                 Serial.write("T1 OK: ");
                 mux_h::tcaselect(0);
                 int result = tof1.readRange();
+				int status = tof1.readRangeStatus();
+				if  ((status >= VL6180X_ERROR_SYSERR_1) && (status <= VL6180X_ERROR_SYSERR_5)) {
+				  Serial.println("System error");
+			  	}
+				else if (status == VL6180X_ERROR_ECEFAIL) {
+				  Serial.println("ECE failure");
+				}
+				else if (status == VL6180X_ERROR_NOCONVERGE) {
+				  Serial.println("No convergence");
+				}
+				else if (status == VL6180X_ERROR_RANGEIGNORE) {
+				  Serial.println("Ignoring range");
+				}
+				else if (status == VL6180X_ERROR_SNR) {
+				  Serial.println("Signal/Noise error");
+				}
+				else if (status == VL6180X_ERROR_RAWUFLOW) {
+				  Serial.println("Raw reading underflow");
+				}
+				else if (status == VL6180X_ERROR_RAWOFLOW) {
+				  Serial.println("Raw reading overflow");
+				}
+				else if (status == VL6180X_ERROR_RANGEUFLOW) {
+				  Serial.println("Range reading underflow");
+				}
+				else if (status == VL6180X_ERROR_RANGEOFLOW) {
+				  Serial.println("Range reading overflow");
+			  	}
                 Serial.print(result);
                 Serial.write("\n");
             }
